3cfd15786c4e68ede071a30463fb722984e5ae7d
PronElle <Massimiliano.Pronesti@gmail.com>
2021-04-18 17:38:52 +0200

    feat: implement getters

--- Lab2/ex3/Directory.cpp
+++ Lab2/ex3/Directory.cpp
@@ -4,4 +4,101 @@
 
 #include "Directory.h"
 
+std::shared_ptr<Directory> Directory::root; // dir's root
 
+Directory::Directory(const std::string &name) : Base(name) {}
+
+/**
+ *
+ * @return
+ */
+int Directory::mType() const {
+    return DIR_t;
+}
+
+/**
+ *
+ * @param name
+ * @param parent
+ * @return
+ */
+std::shared_ptr<Directory> Directory::makeDirectory(std::string name, std::weak_ptr<Directory> parent) {
+    return std::shared_ptr<Directory>();
+}
+
+/**
+ *
+ * @param indent
+ */
+void Directory::ls(int indent) const {
+
+}
+
+/**
+ *
+ * @param name
+ * @return
+ */
+std::shared_ptr<Directory> Directory::addDirectory(const std::string &name) {
+    return std::shared_ptr<Directory>();
+}
+
+/**
+ *
+ * @return
+ */
+std::shared_ptr<Directory> Directory::getRoot() {
+    if(root == nullptr){
+        root = std::shared_ptr<Directory>(new Directory("/"));
+        root->_this = root;
+        root->father = root;
+    }
+
+    return root;
+}
+
+/**
+ *
+ * @param nome
+ * @param size
+ * @return
+ */
+std::shared_ptr<File> Directory::addFile(const std::string &nome, uintmax_t size) {
+    return std::shared_ptr<File>();
+}
+
+/**
+ *
+ * @param name
+ * @return
+ */
+std::shared_ptr<Base> Directory::get(const std::string &name) {
+    return name == ".." ?
+           father.lock() : name == "." ?
+           _this.lock()  : children.find(name)->second;
+}
+
+/**
+ * retrieves dir of given name
+ * @param name
+ * @return
+ */
+std::shared_ptr<Directory> Directory::getDir(const std::string &name) {
+    return std::dynamic_pointer_cast<Directory>(get(name));}
+
+/**
+ * retrieves file of given name
+ * @param name
+ * @return file
+ */
+std::shared_ptr<File> Directory::getFile(const std::string &name) {
+    return std::dynamic_pointer_cast<File>(get(name));}
+
+/**
+ * deletes children of given name
+ * @param name
+ * @return true if success, false otherwise
+ */
+bool Directory::remove(const std::string &name) {
+    return children.erase(name);
+}
--- Lab2/ex3/Directory.h
+++ Lab2/ex3/Directory.h
@@ -13,23 +13,29 @@
 #include "File.h"
 
 class Directory : public Base {
+    static std::shared_ptr<Directory> root;
     std::string name;
     std::weak_ptr<Directory> father;
     std::weak_ptr<Directory> _this;
     std::unordered_map<std::string, std::shared_ptr<Base>> children;
-    Directory(const std::string &name);
+    explicit Directory(const std::string &name);
 
 public :
-    int mType() const;
+    // overridden methods
+    int mType() const override ;
+    void ls(int indent) const override;
+
     static std::shared_ptr<Directory> makeDirectory(std::string name, std::weak_ptr<Directory> parent);
-    void ls(int indent) const;
-    std::shared_ptr<Directory> addDirectory(const std::string &nome);
+    std::shared_ptr<Directory> addDirectory(const std::string &name);
     static std::shared_ptr<Directory> getRoot();
     std::shared_ptr<File> addFile(const std::string &nome, uintmax_t size);
+
+    // getters
     std::shared_ptr<Base> get(const std::string &name);
     std::shared_ptr<Directory> getDir(const std::string &name);
     std::shared_ptr<File> getFile(const std::string &name);
-    bool remove(const std::string &nome);
+
+    bool remove(const std::string &name);
 };
 
 
